627ba32843070a03ebe9d07a59a01694981be9d0
24nagashreebr <76149504+24nagashreebr@users.noreply.github.com>
2020-12-17 13:40:31 +0530

    Create calculator.java
    calculator method

---
+++ calculator.java
@@ -0,0 +1,8 @@
+Class Claculator{
+          public static int add(int a,int b){
+          int sum=a+b;
+          }
+          public static void main(String args[]){
+          System.out.println(add(10,20));
+         }
+     
